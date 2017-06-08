2970eb0fd6a9a836f3b30e00c40ee11a46aa0567
soulmaru <daniel.mf.souza@gmail.com>
2017-06-07 23:50:25 -0300

    WIP: Input do componente de upload

--- app/components/upload-file.js
+++ app/components/upload-file.js
@@ -1,9 +1,31 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-	actions: {
-		upload() {
-			alert('upload!!');
-		}
-	}
+    actions: {
+        upload() {
+            alert('upload!!');
+        },
+
+        uploadAFile(fileList) {
+        let re = new RegExp('text/*');
+
+        if(re.test(fileList[0].type)) {
+            alert('1');
+            let controller = this.controllerFor('retornocip');
+alert('2');
+            controller.set('fileName', fileList[0].name);
+alert('3');
+            let reader = new FileReader();
+alert('4');
+            reader.onloadend = function() {
+              controller.set('fileUrl', reader.result);
+            };
+alert('5');
+            reader.readAsDataURL(fileList[0]);
+   alert('6');
+          } else {
+            alert('Arquivo deve ser texto. Você tentou carregar: ' + fileList[0].type);
+          }
+        }
+    }
 });
--- app/routes/retornocip.js
+++ app/routes/retornocip.js
@@ -1,7 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-	model() {
-		return ['Header','Linha Detalhe','Trailler'];
-	}
+     actions: {
+        uploadAFile(fileList) {
+            let controller = this.controllerFor('retornocip');
+            controller.set('multiFiles', fileList);
+        }
+    },
+    model() {
+        return ['Header','Linha Detalhe','Trailler'];
+    }
 });
